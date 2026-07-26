d1f7153d3f7317d7e6e7b0fb85363dece7061852
264hf2jztj-ship-it <264hf2jztj@privaterelay.appleid.com>
2026-07-26 22:08:15 +0200

    Step 4.2 - add Supabase public configuration

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+window.CUCINA_HUB_CONFIG = Object.freeze({
+  supabaseUrl: "https://jjyirgqyucdbpibogybh.supabase.co",
+
+  supabasePublishableKey:
+    "sb_publishable_87lcAjHhdxYm95pwbHVXUA_lmxekN34"
+});
